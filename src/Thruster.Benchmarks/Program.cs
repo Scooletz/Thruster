58eaf8515d28d3c6e88bca5059b233df8f07f818
Szymon Kulec <scooletz@gmail.com>
2018-08-05 18:55:50 +0200

    Benchmarking project

---
+++ src/Thruster.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Thruster.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<AgainstSharedPool>();
+        }
+    }
+}
